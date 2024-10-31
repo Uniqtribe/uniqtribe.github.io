83456e2b6f2fbe145a6a3465f8accc47db7040cd
Uniqtribe <repository@uniqtribe.com>
2024-10-31 13:47:21 +0530

    Update uniqtribe-cart.js

--- uniqtribe-cart.js
+++ uniqtribe-cart.js
@@ -1,10 +1,17 @@
     const cartItemElements = document.querySelectorAll('[data-cart-item]');
     if (cartItemElements) {
         cartItemElements.forEach(cartItem => {
-            cartItem.querySelector('ul').style.display = 'none';
-            cartItem.querySelectorAll('.theme-cart-qty-inc-dec').forEach(item => {
-                item.disabled = true;
+            let
+            const liElements = document.querySelectorAll("li");
+            liElements.forEach(li => {
+                if (li.textContent.trim().startsWith("source:")) {
+                    cartItem.querySelector('ul').style.display = 'none';
+                    cartItem.querySelectorAll('.theme-cart-qty-inc-dec').forEach(item => {
+                        item.disabled = true;
+                    });
+                    cartItem.querySelector('[data-zs-quantity]').disabled = true;
+                }
             });
-            cartItem.querySelector('[data-zs-quantity]').disabled = true;
+
         })
     }
