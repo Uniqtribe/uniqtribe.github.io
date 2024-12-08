1619ae0d4970fcd26d5e20d5157aef0880742a5b
Uniqtribe <repository@uniqtribe.com>
2024-12-08 10:44:57 +0530

    Update uniqtribe-product.js

--- uniqtribe-product.js
+++ uniqtribe-product.js
@@ -1395,7 +1395,7 @@ function handleColorClick(swatch) {
     }
 
     updateCustomColor();
-
+    alert("A");
     const img = document.querySelector('img[alt="base-image"]');
     const canvas = document.getElementById('imageCanvas');
     const ctx = canvas.getContext('2d');
