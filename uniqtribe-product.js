3d264cbbd10159edfbd8249ad9f43e4cff06e7f5
blesswinprince <princeblesswin@gmail.com>
2025-02-02 09:03:38 +0530

    Update uniqtribe-product.js

--- uniqtribe-product.js
+++ uniqtribe-product.js
@@ -1027,7 +1027,7 @@ console.log("hiddenSelect.value", hiddenSelect);
 //        const preselectedValue = hiddenSelect.value;
 //        const initialOption = Array.from(customOptions).find(option => option.getAttribute('data-value') === preselectedValue);
 //        if (initialOption) {
-            document.querySelector('#customSelect').querySelectorAll('div[data-value]')[0].classList.add('selected')
+            document.querySelector('#customSelect').querySelectorAll('div[data-value]')[0].click();
 
 // Set initial visual selection
 //        }
