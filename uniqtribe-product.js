1fea1e555254d0ab863b28637f475513c8d554bf
Uniqtribe <repository@uniqtribe.com>
2025-06-24 20:46:55 +0530

    Update uniqtribe-product.js

--- uniqtribe-product.js
+++ uniqtribe-product.js
@@ -715,9 +715,6 @@ function loadSelectionFieldsWithPattern() {
         let x = 0;
         variantRows.forEach(row => {
             let label = row.querySelector('.theme-product-variant-label.theme-custom-field-label');
-		console.log(row);
-		console.log(row.querySelector('.theme-product-varients-row'));
-console.log(row.querySelector('.theme-product-varients-row').getAttribute('data-variant-id'));
 		
             if (label?.textContent.replace("*", "").trim().toLowerCase().startsWith('selection') ) {
                 if (row.querySelector('input').value != '' && selectionValue[i].variant === row.querySelector('.theme-product-varients-row').getAttribute('data-variant-id')) {
