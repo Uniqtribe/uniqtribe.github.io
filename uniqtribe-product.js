d669d141690f60bdfa775c3e4cef1177764ec188
Uniqtribe <repository@uniqtribe.com>
2025-06-25 20:26:06 +0530

    Update uniqtribe-product.js

--- uniqtribe-product.js
+++ uniqtribe-product.js
@@ -663,7 +663,9 @@ function updateFields() {
 
 
 document.querySelector('#swatches-0').click();
-document.querySelector('.customSelect').querySelectorAll('div[data-value]')[0].click();
+//document.querySelector('.customSelect').querySelectorAll('div[data-value]')[0].click();
+	document.querySelectorAll('.custom-select').forEach(s => s.querySelector('div[data-value]')?.click());
+
 }
 
 function loadBasicField() {
