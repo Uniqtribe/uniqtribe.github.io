dea0574b881823aa0dd9b55c031c8e35f8253aa2
Uniqtribe <repository@uniqtribe.com>
2025-09-15 22:22:41 +0530

    Update uniqtribe-product.js

--- uniqtribe-product.js
+++ uniqtribe-product.js
@@ -2230,6 +2230,31 @@ function changeColor(changeColorArray) {
             }
         });
     });*/
+
+    const loader = new THREE.GLTFLoader();
+loader.setMeshoptDecoder(window.MeshoptDecoder);
+
+
+
+	 loader.load(configObject.imageInfo.objectPath, function (gltf) {
+        object = gltf.scene;
+        scene.add(object);
+        object.scale.set(0.1, 0.1, 0.1);
+        const box = new THREE.Box3().setFromObject(object);
+        const center = box.getCenter(new THREE.Vector3());
+        object.position.set(center.x + 1.25, center.y - 0.125, 0);
+        textureLoader = new THREE.TextureLoader();
+
+        textures = configObject.imageInfo.textures.map(textureInfo => {
+            const texture = textureLoader.load(textureInfo.texturePath);
+            texture.generateMipmaps = true;
+            texture.minFilter = THREE.LinearMipmapLinearFilter; // Use linear mipmaps for better quality during downscaling
+            return {
+                texture: texture,
+                objects: textureInfo.objects,
+                transparent: textureInfo.transparent
+            };
+        });
 const totalSlices = 5;
 
 const nailSliceMap = {
